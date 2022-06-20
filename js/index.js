e0eaac18b6d37768796440b0d3a4503b4dc83878
Julia <103965884+yulred@users.noreply.github.com>
2022-06-20 21:56:06 +0300

    Milestone 3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-let x = 10;
-let y = 55;
+const button = document.querySelector("button");
+let x;
 
-function fibonacci() {
-    document.getElementById("fibonacci").innerText = `The Fibonacci of ${x} is ${y}`
+function fibonacci(x) {
+
+    button.addEventListener("click", function() {
+
+    let y = 1;
+    let n = 0;
+    let f = 0;
+
+    x = document.getElementById("userinput").value;
+
+    for (let i = 2; i <= x; i++) {
+        f = n + y;
+        n = y;
+        y = f;
+    }
+
+    document.getElementById("fibonacci").innerHTML = `<strong><u>${y}</u></strong>`;
+});
 }
 
-fibonacci();
+fibonacci(x);
